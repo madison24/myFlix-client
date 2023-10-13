c278d80a9d35552d07b460e1d6d64a10d80e9011
madison24 <mhousman24@gmail.com>
2023-10-12 21:26:28 -0500

    updated with api

--- src/Components/main-view/main-view.jsx
+++ src/Components/main-view/main-view.jsx
@@ -1,48 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { response } from "express";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Pride and Prejudice",
-      description:
-        "Sparks fly when spirited Elizabeth Bennet meets single, rich, and proud Mr. Darcy. But Mr. Darcy reluctantly finds himself falling in love with a woman beneath his class. Can each overcome their own pride and prejudice?",
-      genre: "Historical Romance",
-      director: "Joe Wright",
-      actors: "Keira Knightley",
-      featured: true,
-      imagePath:
-        "https://m.media-amazon.com/images/M/MV5BMTA1NDQ3NTcyOTNeQTJeQWpwZ15BbWU3MDA0MzA4MzE@._V1_.jpg",
-    },
-    {
-      id: 2,
-      title: "Bridesmaids",
-      description:
-        "Competition between the maid of honor and a bridesmaid, over who is the bride's best friend, threatens to upend the life of an out-of-work pastry chef.",
-      genre: "Comedy",
-      director: "Judd Apatow",
-      actors: "Kristen Wiig",
-      featured: true,
-      imagePath:
-        "https://m.media-amazon.com/images/M/MV5BMjAyOTMyMzUxNl5BMl5BanBnXkFtZTcwODI4MzE0NA@@._V1_.jpg",
-    },
-    {
-      id: 3,
-      title: "Promising Young Woman",
-      description:
-        "A woman seeks vengeance on those who played a role in her traumatizing past.",
-      genre: "Thriller",
-      director: "Emerald Fennell",
-      actors: "Carey Mulligan",
-      featured: true,
-      imagePath:
-        "https://m.media-amazon.com/images/M/MV5BOTgzMzE4MGItZDgxYS00ZGEwLWE3YTctZWY3ZDAyMTk0ZGU4XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://madison24.github.io/movieAPI/")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            genre: doc.genre_name?.[0],
+            director: doc.director_name?.[0],
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
--- src/Components/movie-card/movie-card.jsx
+++ src/Components/movie-card/movie-card.jsx
@@ -1,3 +1,7 @@
+// import the PropTypes Library
+import PropTypes from "prop-types";
+
+// The moviecard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +12,25 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+// Here is where we define all the props constraints for the MovieCard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string,
+      description: PropTypes.string,
+    }),
+    director: PropTypes.shape({
+      name: PropTypes.string,
+      bio: PropTypes.string,
+      birth: PropTypes.string,
+      death: PropTypes.string,
+    }),
+    actors: PropTypes.string,
+    featured: PropTypes.string,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
